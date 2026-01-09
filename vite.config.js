0bb4708c5471e14fe06cd989eec8860d172c020a
denys-shatin <denys_shatin@proton.me>
2026-01-09 19:07:18 +0200

    fix: base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   plugins: [svelte()],
-  base: '/whyheavens/',
+  base: '/',
   publicDir: 'static',
   build: {
     outDir: 'dist'
